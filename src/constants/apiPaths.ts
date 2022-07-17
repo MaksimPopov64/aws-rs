f27df64463961e29ea0ad31a4c34e32560833040
Maksim_Popov <mpopov@infoblox.com>
2022-07-17 19:30:44 +0300

    feat: added endpoints

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,7 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  products: 'https://fon3pkjz3m.execute-api.eu-west-1.amazonaws.com/dev/products',
+  product: 'https://fon3pkjz3m.execute-api.eu-west-1.amazonaws.com/dev/product/',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
