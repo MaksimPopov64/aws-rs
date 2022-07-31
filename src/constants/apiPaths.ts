0878ce0e8ce7ba612d2dcfb0241c90a9e9bc6764
Maksim_Popov <mpopov@infoblox.com>
2022-07-31 20:56:29 +0300

    fix: fixed config

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,8 @@
 
 const API_PATHS = {
-  products: 'https://fon3pkjz3m.execute-api.eu-west-1.amazonaws.com/dev/products',
-  product: 'https://fon3pkjz3m.execute-api.eu-west-1.amazonaws.com/dev/product/',
+  products: 'https://bq593amx1e.execute-api.eu-west-1.amazonaws.com/dev/products',
+  product: 'https://bq593amx1e.execute-api.eu-west-1.amazonaws.com/dev/product/',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
